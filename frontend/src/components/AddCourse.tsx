9d59fba10ffb588f30e258a5f781a8606fc60a56
algispetlin <apetlin@umass.edu>
2023-11-11 18:03:02 -0500

    more drawer shit
    drawer shit

---
+++ frontend/src/components/AddCourse.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './AddCourse.css';
+
+function AddCourse() {
+    return (
+        <></>
+    );
+}
+
+export default AddCourse;
--- frontend/src/components/Drawer.tsx
+++ frontend/src/components/Drawer.tsx
@@ -1,123 +1,48 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import './Drawer.css';
+
+import { Box, Drawer, IconButton }  from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-
-import IconButton from '@mui/material/IconButton';
-
 import { ClickAwayListener } from '@mui/base/ClickAwayListener';
-import { SxProps } from '@mui/system';
 
-type Anchor = 'top' | 'left' | 'bottom' | 'right';
+import CourseList from './CourseList';
+import AddCourse from './AddCourse';
 
-export default function TemporaryDrawer(props: { select: (index: number) => void, courses: string[] }) {
-  const [state, setState] = React.useState({
-    left: false,
-  });
-
+export default function CourseDrawer(props: { select: (index: number) => void, courses: string[] }) {
+  const [isOpen, setIsOpen] = React.useState(false);
   const [selectedIndex, setSelectedIndex] = React.useState(0);
 
-  const handleListItemClick = (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
-    index: number,
-  ) => {
-    setSelectedIndex(index);
-    props.select(index);
-    //handleClick();
-  };
-
-  const [openDrawer, setOpen] = React.useState(false);
-
-  const handleClick = () => {
-    setOpen((prev) => !prev);
-  };
-
-  const handleClickAway = () => {
-    setOpen(false);
-  };
-
-  const toggleDrawer =
-    (anchor: Anchor, open: boolean) =>
-    (event: React.KeyboardEvent | React.MouseEvent) => {
-      if (
-        event.type === 'keydown' &&
-        ((event as React.KeyboardEvent).key === 'Tab' ||
-          (event as React.KeyboardEvent).key === 'Shift')
-      ) {
-        return;
-      }
-
-      setState({ ...state, [anchor]: open });
-    };
-
-  const styles: SxProps = {
-    position: 'absolute',
-    top: 28,
-    right: 0,
-    left: 0,
-    zIndex: 1,
-    border: '1px solid',
-    p: 1,
-    bgcolor: 'background.paper',
-  };
-
-  const list = (anchor: Anchor) => (
-    <ClickAwayListener onClickAway={handleClickAway}>
-    <Box
-      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
-      role="presentation"
-      //onClick={toggleDrawer(anchor, openDrawer)}
-      //onKeyDown={toggleDrawer(anchor, openDrawer)}
-    >
-      <List>
-        {props.courses.map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton sx={{
-          "&.Mui-selected": {
-            backgroundColor: "#a1a1a1"
-          },
-          "&.Mui-focusVisible": {
-            backgroundColor: "#a1a1a1"
-          },
-          ":hover": {
-            backgroundColor: "#a1a1a1"
-          }
-        }} selected={selectedIndex === index} onClick={(event) => handleListItemClick(event, index)}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-    </ClickAwayListener>
+  const list = () => (
+    <div className="drawer-container">
+      <ClickAwayListener onClickAway={() => { setIsOpen(false) } }>
+      <Box sx={{ width: 300 }} role="presentation">
+        <CourseList 
+          courses={props.courses}
+          selectedIndex={selectedIndex}
+          onClick={(index: number) => { setSelectedIndex(index); props.select(index); }}
+        />
+        <AddCourse />
+      </Box>
+      </ClickAwayListener>
+    </div>
   );
   
   return (
     <div>
-      {(['left'] as const).map((anchor) => (
-        <React.Fragment key={anchor}>
-          <IconButton sx={{ color: "#FFBF00", ml: 1}} aria-label="Menu" onClick={toggleDrawer(anchor, true)}><MenuIcon /></IconButton>
-          <Drawer
-            anchor={anchor}
-            open={state[anchor]}
-            onClose={toggleDrawer(anchor, false)}
-          >
-            {list(anchor)}
-          </Drawer>
-        </React.Fragment>
-      ))}
+      <div className="drawer-title" onClick={() => { setIsOpen(true) }}>
+        <IconButton sx={{ color: "#E5E5E5", mt: 0.2, ml: 1 }} aria-label="Menu">
+          <MenuIcon />
+        </IconButton>
+        Select Classes
+      </div>
+      <div style={{ flex: 1 }}></div>
+      <Drawer
+        anchor={'left'}
+        open={isOpen}
+        onClose={() => { setIsOpen(false) }}
+      >
+        { list() }
+      </Drawer>
     </div>
   );
 }
