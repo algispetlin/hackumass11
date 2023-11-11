10d338f60d67ddd5c9f8fc438d603b83feedf6f1
Professor <99095392+ProfessorSushi@users.noreply.github.com>
2023-11-11 01:52:28 -0500

    Dropdown/Profile
    Added dropdown funcitonality. Omg took so long. Fixed profile.

--- frontend/src/components/HomeHeader.tsx
+++ frontend/src/components/HomeHeader.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './HomeHeader.css';
 import AccountMenu from '../components/Profile';
+import TemporaryDrawer from '../components/Drawer'
 
 interface HomeHeaderProps {
     courseName: string;
@@ -10,12 +11,12 @@ function HomeHeader(props: HomeHeaderProps) {
   return (
     <div className="home-header">
         <div className='corner'>
-
+          <TemporaryDrawer />
         </div>
         <div className='course-name'>{props.courseName}</div>
         <div className='corner'>
-            <AccountMenu />
         </div>
+        <><AccountMenu /></>
     </div>
   );
 }
