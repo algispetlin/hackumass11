4db3a301d75898cb34d23a4912c49d0cb16c563f
attilapalabiyik <123512351+attilapalabiyik@users.noreply.github.com>
2023-11-12 01:16:48 -0500

    Finished database and services
    The database functions should be secure, and will all be using post() from services to be called.

--- frontend/src/models/ApiModel.ts
+++ frontend/src/models/ApiModel.ts
@@ -1,16 +1,55 @@
 export interface Question {
     courseId: string;
     userId: string;
-
     question: string;
+}
+
+export interface newAccount {
+    name: string;
+    email: string;
+}
+
+export interface newCourse {
+    userId: string;
+    name: string;
+    syllabus: string;
+}
+
+export interface UserCourseId {
+    userId: string;
+    courseId: string;
+}
+
+export interface userId {
+    userId: string;
+}
+
+export interface courseId {
+    courseId: string;
+}
+
+export interface changeSyllabus {
+    courseId: string;
+    syllabus: string;
+}
+
+export interface courseIdName {
+    courseId: string;
+    name: string;
+}
+
+export interface changePermission {
+    userId: string;
+    permission: string;
 }
 
 export interface UserSchema {
     _id: string;
-    permissions: string[];
+    permissions: string;
     courses: Course[];
     name: string;
     email: string;
+    prevHighlight: string;
 }
 
 export interface Course {
