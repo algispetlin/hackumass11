3a05a413b9070939f1a1ba2e792c59d26d3d6412
algispetlin <apetlin@umass.edu>
2023-11-12 04:51:56 -0500

    Emergency Fix
    Quick fix

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,15 +3,13 @@ import HomeHeader from '../components/HomeHeader';
 import HomeHeaderMUI from '../components/HomeHeaderMUI';
 import HomePageBody from '../components/HomePageBody';
 
-import { useThemeContext } from "../theme/ThemeContextProvider";
-
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { useThemeContext } from "../theme/ThemeContextProvider";
 import { AuthenticateUser } from '../components/UserInfo';
 
 function HomePage() {
   const { theme } = useThemeContext();
-  
+
   return (
     <>
       <ThemeProvider theme={theme}>
